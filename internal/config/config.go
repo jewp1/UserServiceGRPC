c04b041b25bb35bdda5012eb70d936480844bb3c
SergeyE <evsyukovsergey01@gmail.com>
2025-03-31 12:15:01 +0300

    added work with logger, initialized config

---
+++ internal/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import "time"
+
+const EnvPath = "local.env"
+
+type Config struct {
+	LogLevel   string `envconfig:"logLevel" default:"info"`
+	PostgreSQL PostgreSQL
+	Grpc       GRPC
+}
+
+type PostgreSQL struct {
+	Host                string        `envconfig:"DB_HOST" required:"true"`
+	Port                int           `envconfig:"DB_PORT" required:"true"`
+	Name                string        `envconfig:"DB_NAME" required:"true"`
+	User                string        `envconfig:"DB_USER" required:"true"`
+	Password            string        `envconfig:"DB_PASSWORD" required:"true"`
+	SSLMode             string        `envconfig:"DB_SSL_MODE" default:"disable"`
+	PoolSize            int           `envconfig:"DB_POOL_MAX_CONNS" default:"10"`
+	PoolConnLifeTime    time.Duration `envconfig:"DB_POOL_MAX_CONN_LIFETIME" default:"180s"`
+	PoolMaxConnIdleTime time.Duration `envconfig:"DB_POOL_MAX_CONN_IDLE_TIME" default:"100s"`
+}
+
+type GRPC struct {
+	Port    string        `envconfig:"GRPC_PORT" required:"true"`
+	Timeout time.Duration `envconfig:"GRPC_TIMEOUT" required:"true"`
+}
